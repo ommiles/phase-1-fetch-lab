e5e0c06c5f70e2f713a4e5bc68c0d91bec5d889f
OM Miles <olivia.m.miles@gmail.com>
2021-04-09 08:09:05 -0700

    First commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+  });
   
+  return fetch
 }
 
 function renderBooks(books) {
